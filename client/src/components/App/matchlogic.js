55c9f70767ccd15f5d7c0de9feeed7c13d61d504
prsoulman <60936175+prsoulman@users.noreply.github.com>
2022-08-08 17:16:58 -0500

    added logic

--- client/src/components/App/matchlogic.js
+++ client/src/components/App/matchlogic.js
@@ -6,17 +6,12 @@
 //console.log(`Hi ${localStorageArr} is in localStorage.`);
 //temporary variables, to be replaced with array from topfive page
 //TODO: replace with previous page data from selection page
-const userTopFive_1 = [
-  "The Da Vinci Code (Series)",
-  "Forrest Gump",
-  "Toy Story (Series)",
-  "Captain Phillips",
-  "Apollo 13",
-];
 
-// This array will hold values from previous page of user top five.
-const CurrentLoggedinUser = [...userTopFive_1];
+// This array will hold values from previous page of database top five.
+//const databaseLoggedindatabase = [marvin, savino, jon, jordan, david];
 
+// Variable that Queries the database for top five arrays
+//Const allUser = { movies: []}
 const marvin = [
   "The Da Vinci Code (Series)",
   "Forrest Gump",
@@ -46,50 +41,51 @@ const david = [
   "That Thing You Do!",
 ];
 const savino = [
-  "Forrest Gump",
-  "Sleepless in Seattle",
   "Big",
-  "That Thing You Do!",
-  "Sully",
+  "Saving Private Ryan",
+  "Toy Story",
+  "Forrest Gump",
+  "Cast Away",
 ];
 
-// This array will hold values from previous page of database top five.
-const databaseLoggedindatabase = [marvin, savino, jon, jordan, david];
-
-// Variable that Queries the database for top five arrays
-//Const allUser = { movies: []}
+// This array will hold values from previous page of user top five.
+//const CurrentLoggedinUser = [...userTopFive_1];
+const userTopFive_1 = [
+  "The Da Vinci Code (Series)",
+  "Forrest Gump",
+  "Toy Story (Series)",
+  "Captain Phillips",
+  "Apollo 13",
+];
 
 const userTopFiveCompare = () => {
   // console.log(CurrentLoggedinUser);
   // console.log(databaseLoggedindatabase);
-  let jon = jon;
-  let marvin = marvin;
+userTopFive_1.sort();
+//CurrentLoggedinUser.sort();
   jon.sort();
   marvin.sort();
   //let foundMatches = 0;
 
   //for (let i = 0; i < CurrentLoggedinUser.length; i++) {
-  if (jon === marvin) {
-    //   //foundMatches++;
-    //   console.log("you matched with Marvin");
-    // } else if (userTopFive_1 === savino) {
-    //   //foundMatches++;
-    //   console.log("you matched with Savino");
-    // } else if (userTopFive_1 === jordan) {
-    //   //foundMatches++;
-    //   console.log("you matched with Jordan");
-    // } else if (userTopFive_1 === jon) {
-    //   //foundMatches++;
-    //   console.log("you matched with Jon");
-    // } else (userTopFive_1 === david)
-    // //foundMatches++;
-    // console.log(
-    //   "you matched with david....but he's married...so good luck out there"
-    // );
-
-    console.log("match");
-    return true;
-  }
+  if (jon.length === marvin.length) {
+    console.log("you matched with Marvin")
+  } else if (userTopFive_1 === savino) {
+    //foundMatches++;
+    console.log("you matched with Savino");
+  } else if (userTopFive_1 === jordan) {
+    //foundMatches++;
+    console.log("you matched with Jordan");
+  } else if (userTopFive_1 === jon) {
+    //foundMatches++;
+    console.log("you matched with Jon");
+  } else userTopFive_1 === david;
+  //foundMatches++;
+  console.log(
+    "you matched with david....but he's married...so good luck out there"
+  );
+  console.log("match");
+  return true;
 };
 
 //console.log(CurrentLoggedinUser)
